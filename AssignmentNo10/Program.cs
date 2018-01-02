59d0d8628e2a9abb93dc15cd849f3d2e313c5fa9
ahmadmohey <ahmadmohey@gmail.com>
2018-01-02 17:39:58 +0200

    Complete C# - AssignmentNo10 Solution

---
+++ AssignmentNo10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentNo10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+         // Auto generated method to perform caesar cipher encryption
+        public static char[] Encrypt(string plainText, int shift)
+        {
+            char[] plainArray = plainText.ToLower().ToCharArray();
+            char[] cipherArray = new char[plainArray.Length];
+
+            for (int i = 0; i < plainArray.Length; i++)
+            {
+                char letter = plainArray[i];
+                if (letter != ' ')
+                {
+                    letter = (char)(letter + shift);
+                    if (letter > 'z')
+                    {
+                        letter = (char)(letter - 26);
+                    }
+                    else if (letter < 'a')
+                    {
+                        letter = (char)(letter + 26);
+                    }
+                    cipherArray[i] = letter;
+                }
+            }
+            return cipherArray;
+        }
+    }
+}
